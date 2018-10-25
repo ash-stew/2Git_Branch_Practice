6c27b0fe40ed12f4040fdd6522d7f274c2aa40f6
Ashley <ashleystewart1991@hotmail.co.uk>
2018-10-25 10:20:28 +0100

    Modified button message from 'Exit Program' to 'Exit Application'

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -55,7 +55,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(115, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Exit Program";
+            this.button2.Text = "Exit Application";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
